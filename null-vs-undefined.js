18a0d55e3141b07ebfce93775a85ee05bd56fd01
Israfil-curious <67516436+Israfil-curious@users.noreply.github.com>
2020-08-12 20:41:35 +0600

    done

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return 
+// }
+
+// const result = add(10, 30);
+// console.log(result);
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+add(10);
+
+const developer = { name: "Israfil", phone: 787987 };
+console.log(developer.id, developer.name);
