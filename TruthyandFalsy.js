fe909685c5816016e6b189e6ef38391ceaeb839d
Israfil-curious <67516436+Israfil-curious@users.noreply.github.com>
2020-08-12 20:18:14 +0600

    done

---
+++ TruthyandFalsy.js
@@ -0,0 +1,13 @@
+//falsy value:
+// "", NaN, undefined, 0, false;
+
+//truthy value:
+//" ", {}, [], true;
+
+
+const name = false;
+if (name) {
+    console.log("the name is true");
+} else {
+    console.log("the name is false");
+}
